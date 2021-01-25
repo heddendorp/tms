9754997b7bbf60690ae0e2583d1fbe5f37329eb0
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-01-25 15:22:49 +0100

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+import { AppModule } from '@tms/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// Logic to apply dark mode based on user preference
+// if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+//   document.documentElement.classList.add('dark')
+// } else {
+//   document.documentElement.classList.remove('dark')
+// }
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err: any) => console.error(err));
