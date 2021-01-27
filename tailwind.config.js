e35f7e77a685e725703dfb1d8aec5006442c19da
Lukas Heddendorp <lu.heddendorp@gmail.com>
2021-01-27 20:19:08 +0100

    Add application information

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   prefix: '',
   purge: {
     content: ['./src/**/*.{html,ts}'],
   },
   darkMode: 'class', // or 'media' or 'class'
+  corePlugins: {
+    flex: false,
+    flexDirection: false,
+    flexGrow: false,
+    flexShrink: false,
+    flexWrap: false,
+    gridAutoColumns: false,
+    gridAutoFlow: false,
+    gridAutoRows: false,
+    gridColumn: false,
+    gridColumnEnd: false,
+    gridColumnStart: false,
+    gridRow: false,
+    gridRowEnd: false,
+    gridRowStart: false,
+    gridTemplateColumns: false,
+    gridTemplateRows: false,
+  },
   theme: {
-    extend: {},
+    extend: { colors: { gray: colors.blueGray } },
   },
   variants: {
     extend: {},
