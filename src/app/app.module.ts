b08e81704b0ca178f8aafa5b8804bf4e347ebd29
Lukas Heddendorp <lu.heddendorp@gmail.com>
2022-07-02 12:02:05 +0200

    feat: use svg icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { IconUrlPipe } from './icon-url.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, IconUrlPipe],
   imports: [BrowserModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
